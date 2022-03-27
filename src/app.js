54fe370ccc6500d614db487972b80e944d882550
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-03-27 16:04:01 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css'
+ReactDOM.render(<p>this is my boilerplate</p>, document.getElementById('app'));
