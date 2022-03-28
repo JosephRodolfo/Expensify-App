d5487ecf9bbc7acd0a6c2a73092da21aaeff93b2
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-03-27 20:03:49 -0400

    Add initial versions of page components

--- src/app.js
+++ src/app.js
@@ -1,5 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/styles.scss';
-import 'normalize.css/normalize.css'
-ReactDOM.render(<p>this is my boilerplate</p>, document.getElementById('app'));
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import "./styles/styles.scss";
+import "normalize.css/normalize.css";
+
+
+const ExpanseDashboardPage = () => (
+
+    <div>
+        This is from my dashboard component.
+    </div>
+);
+const AddExpensePage = () => (
+
+    <div>
+        This is from my expense component.
+    </div>
+);
+const EditExpensePage = () => (
+
+    <div>
+        This is from my edit component.
+    </div>
+);
+const HelpPage = () => (
+
+    <div>
+        This is from my help component.
+    </div>
+);
+const NotFoundPage = () => (
+
+    <div>
+        404!
+    </div>
+);
+
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={ExpanseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={NotFoundPage} />
+
+        </Switch>
+    </BrowserRouter>);
+ReactDOM.render(routes, document.getElementById("app"));
