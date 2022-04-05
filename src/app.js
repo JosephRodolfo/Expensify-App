755deab67adcbecfa2ce313479050e8e1e03aecc
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-04-05 16:28:01 -0400

    Delete dummy data

--- src/app.js
+++ src/app.js
@@ -13,37 +13,6 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-//console.log(store.getState());
-
-store.dispatch(
-  addExpense({ description: "Water bill", amount: 4500, createdAt: -21000 })
-);
-
-store.dispatch(
-  addExpense({ description: "Gas bill", amount: 175, createdAt: 1000 })
-);
-store.dispatch(
-  addExpense({ description: "Rent", amount: 109500, createdAt: -10000 })
-);
-//store.dispatch(setTextFilter("water"));
-
-
-
-setTimeout(()=> {
- // store.dispatch(setTextFilter("bill"));
-
-}, 3000)
-
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-
-
-
-
-
 const jsx = (
  <Provider store={store}> 
    <AppRouter />
