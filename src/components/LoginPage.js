836d46761327dc10b12b0df94b204b3b85c04b46
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-04-13 13:35:36 -0400

    Add styles, add loading page component and test, fix typo in app.js

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,17 +2,18 @@ import React from "react";
 import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 
-const LoginPage = ({startLogin}) => (
-  <div>
-    <button onClick={startLogin}>Log in</button>
+const LoginPage = ({ startLogin }) => (
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensify App</h1>
+      <p>It's time to get your expenses under control.</p>
+      <button className="button" onClick={startLogin}>Log in with Google</button>
+    </div>
   </div>
 );
 
-
-const mapDispatchToProps = (dispatch)=> ({
-  startLogin: ()=> dispatch(startLogin()),
-
-
-})
+const mapDispatchToProps = (dispatch) => ({
+  startLogin: () => dispatch(startLogin()),
+});
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
